67e14103923e75b6b47fb290fdf5559a1cca0c53
MikeDelaney <mdelaney@utexas.edu>
2014-06-23 12:18:34 -0700

    Create basic flask app

---
+++ schools_app.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from flask import Flask
+from flask import g
+from flask import render_template
+from flask import abort
+from flask import request
+from flask import url_for
+from flask import redirect
+from flask import session
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def main_page():
+    return u'<h1>Hello world!</h1>'
+
+if __name__ == '__main__':
+    app.run(debug=True)
