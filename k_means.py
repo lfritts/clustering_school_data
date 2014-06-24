abandoning K-means for now, actually not best way to get the info we need.  K-means multi-run functionality is WRONG.  It needs to choose the run with the lowest COST, not average the results of multiple runs.

k_means.py
@@ -83,7 +83,7 @@ def compute_centroids(X, idx, K):
     # is True or False depending if example m is part of cluster k.
     idxLogical = np.zeros((K, m))
 
-    # loop over centroids and if set row to T/F depending on
+    # loop over centroids and set row to T/F depending on
     # membership in cluster k.
     for k in range(K):
         idxLogical[k] = (idx == k)