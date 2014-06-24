3027eb7cb62447a7268d472a999dbc2c07c83519
MikeDelaney <mdelaney@utexas.edu>
2014-06-23 22:56:52 -0700

    Add about, main, and results pages

--- schools_app.py
+++ schools_app.py
@@ -13,8 +13,23 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def main_page():
+def cover_page():
     return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/main')
+def main_page():
+    return render_template('main.html')
+
+
+@app.route('/results')
+def results_page():
+    return render_template('results.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
