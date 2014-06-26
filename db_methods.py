e117d3201957eee93e04508e37a052f7a601aafd
lfritts <llfritts@gmail.com>
2014-06-26 11:16:55 -0700

    Fix sql statement so only one call to db

--- db_methods.py
+++ db_methods.py
@@ -17,7 +17,7 @@ schooltype = %s;
 
 DB_GET_SCHOOLS_BY_ID = """
 SELECT buildingid, district, school, enrollment, lowses FROM demographics
-WHERE buildingid = %s;
+WHERE buildingid IN %s;
 """
 
 DB_GET_ID_FOR_SCHOOL = """
@@ -32,7 +32,7 @@ SELECT schooltype FROM demographics where buildingid = %s;
 def connect_db():
     #user only has read access
     con = psycopg2.connect('''
-        host=mpl-schooldata.cp9cgekjzx3g.us-west-2.rds.amazonaws.com
+        host=mpl-schoolappdata.ck0b6t86e1sf.us-west-2.rds.amazonaws.com
         dbname=school_data_2013''')
     return con.cursor()
 
@@ -80,7 +80,10 @@ def get_results(school_name, district, number_to_return):
 def get_schools_by_id(school_ids):
     school_list = []
     cur = connect_db()
-    for sid in school_ids:
-        cur.execute(DB_GET_SCHOOLS_BY_ID, [sid])
-        school_list.append(cur.fetchone())
-    return school_list
+    #schools_tup = tuple(school_ids)
+    try:
+        cur.execute(DB_GET_SCHOOLS_BY_ID, [tuple(school_ids)])
+    except Exception as e:
+        import pdb; pdb.set_trace()
+    school_list.append(cur.fetchall())
+    return school_list[0]
