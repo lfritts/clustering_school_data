2f8569a44a09809c4f5432fc14465c6da4c6dc72
MikeDelaney <mdelaney@utexas.edu>
2014-06-27 10:58:53 -0700

    Disable debug, enable gevent

--- schools_app.py
+++ schools_app.py
@@ -60,6 +60,6 @@ def contact():
 
 
 if __name__ == '__main__':
-    # http_server = WSGIServer(('', 5000), app)
-    # http_server.serve_forever()
-    app.run(debug=True)
+    http_server = WSGIServer(('', 5000), app)
+    http_server.serve_forever()
+    # app.run(debug=True)
