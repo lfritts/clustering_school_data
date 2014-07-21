6256caa5ba403facfcd3cb9053468110623cd57d
Perry Hook <perry.hook@gmail.com>
2014-07-21 11:25:18 -0700

    basic test for _prep_k_data written.  _pret_k_data seems to be working

--- test_school_k_means.py
+++ test_school_k_means.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import school_k as k
-from school_k import NoSchoolWithIDError
 # import numpy as np
 import pytest
 slow = pytest.mark.slow
@@ -15,7 +14,7 @@ the k_means module, so that is tested here.
 
 @pytest.fixture(scope="module")
 def data_tuples():
-    scData = pd.DataFrame.from_csv('test_data/k_nrml_demographics_data.txt')
+    scData = pd.DataFrame.from_csv('test_data/k_nrml_demographics_data.csv')
     ids = scData[:]['School_ID'].values
     enroll = scData[:]['NRML_Enroll'].values
     lowSES = scData[:]['Low_SES'].values
@@ -23,27 +22,22 @@ def data_tuples():
     return tuples
 
 
-def test_prep_k_data_Error(data_tuples):
-    # 1111 is not in the data
-    search_ID = 1111
-    with pytest.raises(NoSchoolWithIDError):
-        k._prep_k_data(search_ID, data_tuples)
-
-
 def test_prep_k_data(data_tuples):
+    id_list, data_array = k._prep_k_data(data_tuples)
+    assert len(id_list) == 2305
+    assert len(data_array) == 2305
     # Edmonds Challenge Elementary ID is 1520 in the data row 8
-    search_ID = 1520
-    tuple = k._prep_k_data(search_ID, data_tuples)
-    assert tuple[0] == 8
-    assert tuple[1][8] == search_ID
-    # below is third in tuple, 8th row, first column enrollment
-    assert tuple[2][8][0] == 13.03571429
-    # Challenge Elementary % low SEs is 6.21
-    assert tuple[2][8][1] == 6.21
-    # Wa He Lut Indian Elementary ID is 8407
-    assert tuple[1][2304] == 8407
+    assert id_list[8] == 1520
+    # Challenge Elementary NRML_Enroll = 13.03571429
+    assert data_array[8][0] == 13.03571429
+    # Challenge Elementary % Low_SES = 6.21
+    assert data_array[8][1] == 6.21
+    # # Wa He Lut Indian Elementary ID is 8407
+    assert id_list[2304] == 8407
+    # Wa He Lut NRML_Enroll = 5.758928571
+    assert data_array[2304][0] == 5.758928571
     # Wa He Lut % low SES is 7.27
-    assert tuple[2][2304][1] == 7.27
+    assert data_array[2304][1] == 7.27
 
 """
 # for some reason this slow test runs all the time???
