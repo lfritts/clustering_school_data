db2574c0b8474b751c84a981f0eb5f7a33855c21
MikeDelaney <mdelaney@utexas.edu>
2014-07-21 17:37:27 -0700

    Change tests to reflect changes to design of get_latlong

--- test_get_latlong.py
+++ test_get_latlong.py
@@ -2,17 +2,48 @@ from get_latlong import get_latlong
 
 
 def test_get_latlong_for_school():
-    address = 'Almira Elementary School 99103'
-    actual = get_latlong(address)
-    expected_lat = 47.7078193
-    expected_long = -118.9408949
-    assert actual[0] == expected_lat
-    assert actual[1] == expected_long
+    record = {'City': 'Aberdeen',
+              'District name': 'Aberdeen School District',
+              'ZipCode': '98520-5510',
+              'Lowest Grade': 'PK',
+              'School Code': '2834',
+              'phone': '360.538.2131',
+              'State': 'Washington',
+              'AddressLine2': '',
+              'AddressLine1': '1801 Bay Ave.',
+              'District-County Code': '14005',
+              'Principal Name': "William O'Donnell",
+              'ESD Name': 'Educational Service District 113',
+              'School Type': 'Public School',
+              'Highest Grade': '6',
+              'School name': 'A J West Elementary',
+              'ESD Code': '34801'}
+    actual = get_latlong(record)
+    expected_lat = '46.971922'
+    expected_long = '-123.838252'
+    assert actual['Lat'] == expected_lat
+    assert actual['Long'] == expected_long
 
 
 def test_get_latlong_notfound():
-    # create bad address to generate NotFound message
-    address = 'akdjhfakdjsf'
-    actual = get_latlong(address)
+    # create bad address, zip to generate NotFound message
+    record = {'City': 'Aberdeen',
+              'District name': 'Aberdeen School District',
+              'ZipCode': 'asdfkjaf',
+              'Lowest Grade': 'PK',
+              'School Code': '2834',
+              'phone': '360.538.2131',
+              'State': 'Washington',
+              'AddressLine2': '',
+              'AddressLine1': 'adjfhlkajdshf',
+              'District-County Code': '14005',
+              'Principal Name': "William O'Donnell",
+              'ESD Name': 'Educational Service District 113',
+              'School Type': 'Public School',
+              'Highest Grade': '6',
+              'School name': 'A J West Elementary',
+              'ESD Code': '34801'}
+    actual = get_latlong(record)
     expected = 'NotFound'
-    assert actual == expected
+    assert actual['Lat'] == expected
+    assert actual['Long'] == expected
