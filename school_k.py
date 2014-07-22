dd2da5e72a2e469c5d9239a6d8b6a85343ed6a45
Perry Hook <perry.hook@gmail.com>
2014-07-21 17:42:16 -0700

    first refactoring in prep for parallel approach.  test changed to match, still passes.

--- school_k.py
+++ school_k.py
@@ -9,12 +9,11 @@ def _prep_k_data(data):
     * data (a list of tuples). The first element of the tuple is always the
     building id for the school represented in the data in the rest of the
     tuple.
-    * returns (id_list, data_array)
+    * returns id_list, data_array
         -a list of the school ids, and
         - a numpy array of the data
     """
-    # initialize numpy array data_array to all zeros - probably faster than
-    # vstack
+    # init numpy array data_array to all zeros - probably faster than vstack
     num_schools = len(data)
     num_features = len(data[0]) - 1
     id_list = []
@@ -26,48 +25,46 @@ def _prep_k_data(data):
     return id_list, data_array
 
 
-def find_school_clusters(data, K=5, tol=0, max_iters=60, num_runs=5):
+def find_best_school_clusters(
+        data, K=5, tol=0, max_iters=60, num_runs=5, debug=False):
     """
-    find_schools_in_cluster will take a
+    find_best_school_clusters takes
     * data (a list of tuples). The first element of the tuple is always the
     building id for the school represented in the data in the rest of the
     tuple.
     * K, the number of clusters the data will be grouped into, by default 5.
-    * tol is the tolerance for a convergence test.  In the update step, if the
-    new centroids squared distance from the previous centroids is less than or
-    equal to the tol, K-means will stop iterating.
+    * tol is the tolerance for a convergence test that stops iterations.
     * max_iters is the number of iterations K-means algorithm uses to update
     centroids. Defaults to 5.
     * num_runs is the number of times this method runs K-means and chooses
     the lowest cost run of them.
-
-    - returns a list of lists. The outer list contains K inner lists, one
+    Returns a list of lists. The outer list contains K inner lists, one
       for each cluster.  Each inner list contains the school ids of
       schools in a given cluster.
     """
     id_list, X = _prep_k_data(data)
     # do K-Means num_runs times, updating best run when necessary
-    # best_centroids = None
-    best_idx = None
+    best_cluster_idx = None
+    best_centroids = None
     lowest_cost = float('inf')
-    for i in range(num_runs):
-        # print "In run {}\n".format(i)
-        init_centroids = km.init_centroids(X, K)
-        centroids, idx = km.run_k_means(X, init_centroids, max_iters, tol)
-        cost = km.cost(centroids, X, idx)
-        print "Cost for run {0} is {1}\n".format(i, cost)
+    for run in range(num_runs):
+        cost, cluster_idx, centroids = get_k_run_results(X, K, max_iters, tol)
         if cost < lowest_cost:
-            # best_centroids = centroids
-            best_idx = idx
+            best_cluster_idx = cluster_idx
             lowest_cost = cost
-        # print "K-Means Run {} finished. Best centroids so far are:".format(i)
-        # print best_centroids
-        # print "With cost: {}".format(lowest_cost)
-        # raw_input("Press enter to continue...")
+            best_centroids = centroids
+            if debug:
+                print_debug_info_and_pause(
+                    run, cost, centroids, lowest_cost, best_centroids)
+    # find the schools in each cluster and build the return list
+    return build_clusters(id_list, best_cluster_idx, K)
 
-    # for each centroid, find the schools in that cluster and build the
-    # return list
-    return build_clusters(id_list, best_idx, K)
+
+def get_k_run_results(X, K, max_iters, tol):
+    init_centroids = km.init_centroids(X, K)
+    centroids, cluster_idx = km.run_k_means(X, init_centroids, max_iters, tol)
+    cost = km.cost(centroids, X, cluster_idx)
+    return cost, cluster_idx, centroids
 
 
 def build_clusters(ids, cluster_idx, K):
@@ -77,3 +74,14 @@ def build_clusters(ids, cluster_idx, K):
     for school_idx, cluster in enumerate(cluster_idx):
         return_list[cluster].append(ids[school_idx])
     return return_list
+
+
+def print_debug_info_and_pause(
+        run, cost, centroids, lowest_cost, best_centroids):
+        print "K-Means Run {} done."
+        print "Cost for run {0} is {1} with centroids:".format(run, cost)
+        print centroids
+        print "Best centroids so far are:".format(run)
+        print best_centroids
+        print "With cost: {}".format(lowest_cost)
+        raw_input("Press enter to continue...")
