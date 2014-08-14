000aa633be78986ea650a03526c08a2209920828
MikeDelaney <mdelaney@utexas.edu>
2014-08-14 10:48:19 -0700

    Add preventDefault to submit to force return to page after invalid input

--- static/js/dist_script.js
+++ static/js/dist_script.js
@@ -1,8 +1,9 @@
 $(document).ready (function() {
     $("#district").quickselect({data: districtList});
-    $("#submit").click(function() {
+    $("#submit").click(function(e) {
       var selectedDistrict = $("#district").val();
       if ($.inArray(selectedDistrict, districtList) == -1) {
+        e.preventDefault();
         alert("You must select a valid district");
       } else {
         $("#dist_form").submit()
