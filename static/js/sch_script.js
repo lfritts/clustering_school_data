8894e68ea314942cc086d76a8362ae90e15273a4
MikeDelaney <mdelaney@utexas.edu>
2014-08-14 13:58:38 -0700

    Add logic to prevent submission of form with invalid number of schools

--- static/js/sch_script.js
+++ static/js/sch_script.js
@@ -6,6 +6,11 @@ $(document).ready (function() {
       if ($.inArray(selectedSchool, schoolList) == -1) {
         e.preventDefault();
         alert("You must select a valid school");
+      } else if (Math.floor(numSchools) != numSchools) {
+        console.log(typeof(numSchools));
+        console.log(Math.floor(numSchools));
+        e.preventDefault();
+        alert("Please enter a valid number");
       } else {
         $("#school_form").submit()
       }
